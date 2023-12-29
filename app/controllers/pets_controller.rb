2a0d07c482b1e840bba31cc729242e5a96fae2b4
brian <riceb53@gmail.com>
2023-12-29 09:42:46 -0800

    add user_id to pets create

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -7,7 +7,8 @@ class PetsController < ApplicationController
       @pet = Pet.new(
         name: params[:name],
         age: params[:age],
-        breed: params[:breed]
+        breed: params[:breed],
+        user_id: current_user.id
       )
       @pet.save
       render :show
